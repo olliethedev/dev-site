b9dcf148057881256ed91e22a9b9a3c8608885c4
Ollie <3martynov@gmail.com>
2021-04-28 00:03:54 -0400

    clean up

--- src/App.js
+++ src/App.js
@@ -1,37 +1,20 @@
-import logo from './ollie.png';
-import git from './github.ico';
-import './App.css';
+import logo from "./ollie.png";
+import git from "./github.ico";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Ollie Codes
-        </h1>
-        <a
-          className="App-link"
-          href="https://blog.olliecodes.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h5>Blog</h5>
+        <h1>Ollie Codes</h1>
+        <a className="App-link" href="https://blog.olliecodes.com">
+          <b>Blog</b>
         </a>
-        <a
-          className="App-link"
-          href="https://blog.olliecodes.com/portfolio"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h5>Portfolio</h5>
+        <a className="App-link" href="https://blog.olliecodes.com/portfolio">
+          <b>Portfolio</b>
         </a>
-        <a
-          className="App-link"
-          href="https://github.com/olliethedev"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://github.com/olliethedev">
           <img src={git} alt="github" />
         </a>
       </header>
